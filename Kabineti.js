8dccbe8670131668a7feee9f9a6ea940e53df24f
Svetlana Zilpausha <Zilpausha@inbox.lv>
2021-02-23 10:55:33 +0200

    Pievienoti skolas nosaukumi

---
+++ Kabineti.js
@@ -0,0 +1,32 @@
+const Kabineti = [{
+    "Vards": "111.kab.",
+    "Stavs": "1.",
+    "Atbildigais": "Jānis Berzinsh"
+},
+{
+    "Vards": "121.kab.",
+    "Stavs": "2.",
+    "Atbildigais": "Māris Avotinsh"
+},
+{
+    "Vards": "136.kab.",
+    "Stavs": "1.",
+    "Atbildigais": "Maija Liepa"
+},
+{
+    "Vards": "352.kab.",
+    "Stavs": "3.",
+    "Atbildigais": "Kārlis Krūmiņš"
+},
+{
+    "Vards": "Lielā zāle.kab.",
+    "Stavs": "3.",
+    "Atbildigais": "Andris Vilciņš"
+},
+{
+    "Vards": "Mazā zāle.kab.",
+    "Stavs": "3.",
+    "Atbildigais": "Gatis Lapiņš"
+}
+
+]
---
+++ darbinieki.js
@@ -0,0 +1,32 @@
+const darbinieki = [{
+    "Vards": "Jānis",
+    "Uzvards": "Berzinsh",
+    "Parole": "1"
+},
+{
+    "Vards": "Māris",
+    "Uzvards": "Avotinsh",
+    "Parole": "2"
+},
+{
+    "Vards": "Maija",
+    "Uzvards": "Liepa",
+    "Parole": "3"
+},
+{
+    "Vards": "Kārlis",
+    "Uzvsrds": "Krūmiņš",
+    "Parole": "4"
+},
+{
+    "Vards": "Andris",
+    "Uzvards": "Vilciņš",
+    "Parole": "5"
+},
+{
+    "Vards": "Gatis",
+    "Uzvards": "Lapiņš",
+    "Parole": "6"
+}
+
+]
